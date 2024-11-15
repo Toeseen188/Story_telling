27730d6e19d1442c07d41a34d45bdc3fcfc86f47
Moyinoluwa17 <moyinoluwaadigun390@gmail.com>
2024-11-16 01:38:58 +1300

    frontend secondUpdate

--- CMS/src/Components/Navbar.jsx
+++ CMS/src/Components/Navbar.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-// import logo from '../assets/logo.png';
+import logo from '../assets/logo.png';
 
 const Navbar = ({ user, onLogout }) => {
   return (
-    <nav className="navbar navbar-expand-lg  fixed-top">
+    <nav className="navbar navbar-expand-lg fixed-top custom-navbar">
       <div className="container-fluid">
-        {/* Brand logo and Offcanvas Toggle Button */}
-        {/* <Link className="navbar-brand" href="#">
-          <img src={logo} alt="Logo" className="img-fluid w-25" />
-        </Link> */}
+        <Link className="navbar-brand" href="#">
+          <img src={logo} alt="Logo" className="img-fluid w-25 custom-logo" />
+        </Link>
 
         <button
           className="navbar-toggler"
@@ -17,10 +16,11 @@ const Navbar = ({ user, onLogout }) => {
           data-bs-toggle="offcanvas"
           data-bs-target="#offcanvasNavbar"
           aria-controls="offcanvasNavbar"
-          aria-label="Toggle navigation"  
+          aria-label="Toggle navigation"
         >
           <span className="navbar-toggler-icon"></span>
         </button>
+
         <div
           className="offcanvas offcanvas-end"
           tabindex="-1"
@@ -58,40 +58,45 @@ const Navbar = ({ user, onLogout }) => {
               </li>
             </ul>
 
-           
-          {/* Conditional rendering for logged-in users */}
-          <ul className="navbar-nav ms-auto">
-            {user ? (
-              <>
-                <li className="nav-item">
-                  <Link to="/profile" className="nav-link">
-                    {user.name || 'Profile'}
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <button
-                    onClick={onLogout}
-                    className="nav-link btn btn-link text-white"
-                  >
-                    Logout
-                  </button>
-                </li>
-              </>
-            ) : (
-              <>
-                <li className="nav-item">
-                  <Link to="/user/login" className="nav-link login-link rounded-pill px-4">
-                    Login
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link to="/user/signup" className="nav-link signup-link rounded-pill px-4">
-                    Sign Up
-                  </Link>
-                </li>
-              </>
-            )}
-          </ul> 
+            {/* Conditional rendering for logged-in users */}
+            <ul className="navbar-nav ms-auto">
+              {user ? (
+                <>
+                  <li className="nav-item">
+                    <Link to="/profile" className="nav-link">
+                      {user.name || 'Profile'}
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <button
+                      onClick={onLogout}
+                      className="nav-link btn btn-link text-white"
+                    >
+                      Logout
+                    </button>
+                  </li>
+                </>
+              ) : (
+                <>
+                  <li className="nav-item">
+                    <Link
+                      to="/user/login"
+                      className="nav-link login-link rounded-pill px-4"
+                    >
+                      Login
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link
+                      to="/user/signup"
+                      className="nav-link signup-link rounded-pill px-4"
+                    >
+                      Sign Up
+                    </Link>
+                  </li>
+                </>
+              )}
+            </ul>
           </div>
         </div>
       </div>
