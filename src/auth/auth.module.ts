15e5770e0ce330756d0253d32878a9894d206bf2
Hammed Tosin <Hammedtosin188@gmail.com>
2024-11-17 01:24:19 +0100

    changes

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,14 +5,17 @@ import { UsersModule } from '../users/users.module';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './jwt.strategy';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from '../entities/user.entity'
 
 @Module({
   imports: [
-    UsersModule, 
+    UsersModule,  
+    TypeOrmModule.forFeature([User]),
     PassportModule, 
     JwtModule.register({
-      secret: 'your-secret-key', // Replace with a strong secret key
-      signOptions: { expiresIn: '1d' }, // Token expiration time
+      secret: 'qYHQ6jPyK4xYZ0cD0UafMeJQfRATGP4q9Yzlxxo4IhA=', // Replace with a strong secret key
+      signOptions: { expiresIn: '7d' }, // Token expiration time
     }),
   ],
   providers: [AuthService, JwtStrategy], // Include JwtStrategy
--- src/typeorm.config.ts
+++ src/typeorm.config.ts
@@ -1,5 +1,8 @@
 import { DataSource } from 'typeorm';
-
+import { User } from './entities/user.entity';
+import { Story } from './entities/story.entity';
+import { Comment } from './entities/comment.entity';
+import { Reaction } from './entities/reaction.entity';
 export const AppDataSource = new DataSource({
   type: 'postgres', // or 'mysql'
   host: 'localhost',
@@ -7,6 +10,10 @@ export const AppDataSource = new DataSource({
   username: 'my_story_user',
   password: 'story',
   database: 'my_story_db',
+<<<<<<< HEAD
   entities: [], // we'll add our entities later
+=======
+  entities: [Story, User, Comment, Reaction], // we'll add our entities later
+>>>>>>> parent of e4b3e78... resolving conflict
   synchronize: true, // for development only
 });
