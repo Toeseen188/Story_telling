15f585bfd3f4ec3d4801855f6a743c0f3b159c17
Hammed Tosin <Hammedtosin188@gmail.com>
2024-11-17 01:16:45 +0100

    Revert "trying to resolve conflict O"
    This reverts commit b06e39afff7c8bd279492fab17e567b4c10cd262.

--- src/typeorm.config.ts
+++ src/typeorm.config.ts
@@ -4,9 +4,9 @@ export const AppDataSource = new DataSource({
   type: 'postgres', // or 'mysql'
   host: 'localhost',
   port: 5432, // or 3306 for MySQL
-  username: 'my_story_user',
-  password: 'story',
-  database: 'my_story_db',
+  username: 'your_db_username',
+  password: 'your_db_password',
+  database: 'your_db_name',
   entities: [], // we'll add our entities later
   synchronize: true, // for development only
 });
